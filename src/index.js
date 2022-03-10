9ba1028ddddf6ed47f7b53dfd4bed0f7f33fd74a
Manuel L <mlondono@q10.com>
2022-03-10 17:34:28 -0500

    refactor: change login logic

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 // @ts-check
 require('dotenv').config()
+const { chromium } = require('playwright')
+const { delay } = require('./helpers/delay.js')
 const {
   loginGooglePlayConsole
 } = require('./loginGooglePlayConsole/loginGooglePlayConsole.js')
 
 const main = async () => {
-  await loginGooglePlayConsole()
+  const browser = await chromium.launch({ headless: false })
+
+  await loginGooglePlayConsole(browser)
+
+  await delay({ seg: 60 })
+  await browser.close()
 }
 
 main()
--- src/loginGooglePlayConsole/loginGooglePlayConsole.js
+++ src/loginGooglePlayConsole/loginGooglePlayConsole.js
@@ -1,14 +1,12 @@
 // @ts-check
-const { chromium } = require('playwright')
 const { config } = require('../config/config')
 const { delay } = require('../helpers/delay')
 const { confirmEmail } = require('./step_01_confirmEmail')
 const { confirmPass } = require('./step_02_confirmPass')
 const { selectedAccount } = require('./step_03_selectedAccount')
 
-module.exports.loginGooglePlayConsole = async () => {
-  const browser = await chromium.launch({ headless: false })
-
+/** @param {import('playwright').Browser} browser */
+module.exports.loginGooglePlayConsole = async (browser) => {
   const page = await browser.newPage()
 
   await page.goto(config.pages.login)
@@ -21,7 +19,4 @@ module.exports.loginGooglePlayConsole = async () => {
 
   await selectedAccount(page)
   await delay({ seg: 3 })
-
-  await delay({ seg: 60 })
-  await browser.close()
 }
